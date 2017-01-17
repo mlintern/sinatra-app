4bf4b8e9a0a4c328dbbb19353850df6e3ce044a9
Mark Lintern <mark.lintern@oracle.com>
2017-01-17 12:19:44 -0500

    updated with fontstrap

---
+++ public/dist/js/fontstrap.js
@@ -0,0 +1,35 @@
+function loadScript(url, callback){
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if (script.readyState){ //IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else { //Others
+        script.onload = function(){
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+var currSrc = document.currentScript.src,
+    jsPath = currSrc.substr(0, currSrc.lastIndexOf("/")) + '/'; // "dist/js/";
+
+loadScript(jsPath + "jquery-3.1.1.min.js", function(){
+    loadScript(jsPath + "tether.min.js", function(){
+        loadScript(jsPath + "bootstrap.min.js", function(){
+            $(function () {$('[data-toggle="tooltip"]').tooltip();});
+        });
+    });
+    loadScript(jsPath + "lodash.min.js", function(){ });
+    loadScript(jsPath + "tools.js", function(){ });
+});
